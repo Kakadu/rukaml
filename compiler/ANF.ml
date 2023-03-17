075352ebc61452c81296e35a65a1473e33b79727
Kakadu <Kakadu@pm.me>
2023-03-18 01:06:18 +0300

    ANF conversion works better. Need to pretty print more carefully
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- compiler/ANF.ml
+++ compiler/ANF.ml
@@ -41,79 +41,255 @@ and normalize_term t = normalize t Fun.id
             |  <aexp>
 
  *)
+type atom = [ `Atom ]
+
 type _ expr =
-  | AConst : int -> [ `Atom ] expr
+  | AUnit : [ `Atom ] expr
+  | AConst : Parsetree.const -> [ `Atom ] expr
   | AVar : string -> [ `Atom ] expr
   | APrimitive : [ `Atom ] expr
   | ALam : string * _ expr -> [ `Atom ] expr
   (*  *)
-  | CApp : [ `Atom ] expr * [ `Atom ] expr -> [ `Complex ] expr
-  | CIte : [ `Atom ] expr * [ `Default ] expr * [ `Default ] expr -> [ `Complex ] expr
-  | ELet : Parsetree.rec_flag * string * [ `Complex ] expr * _ expr -> [ `Default ] expr
+  | CApp : [ `Atom ] expr * [ `Atom ] expr -> [ `Complex | `Atom ] expr
+  | CIte :
+      [ `Atom ] expr
+      * [ `Atom | `Complex | `Default ] expr
+      * [ `Atom | `Complex | `Default ] expr
+      -> [ `Complex ] expr
+  | CAtom : [ `Atom ] expr -> [ `Complex ] expr
   (*  *)
-  | EAtom : [ `Atom ] expr -> [ `Default ] expr
+  | ELet :
+      Parsetree.rec_flag
+      * string
+      * [ `Complex | `Atom ] expr
+      * [ `Atom | `Complex | `Default ] expr
+      -> [ `Atom | `Complex | `Default ] expr
   | EComplex : [ `Complex ] expr -> [ `Default ] expr
 
-let pp : 'a. Format.formatter -> 'a expr -> unit = fun ppf e -> Format.fprintf ppf "<anf>"
+type any_expr = [ `Atom | `Complex | `Default ] expr
 
-let rec normalize : 'a 'b. Typedtree.expr -> ([ `Default ] expr -> 'a) -> 'a =
- fun m k ->
+let rec pp : 'a. Format.formatter -> 'a expr -> unit =
+  let open Format in
+  fun (type a) ppf : (a expr -> unit) -> function
+    | AUnit -> fprintf ppf "()"
+    | APrimitive -> fprintf ppf "<prim>"
+    | AConst (Parsetree.PConst_int n) -> pp_print_int ppf n
+    | AConst (Parsetree.PConst_bool n) -> pp_print_bool ppf n
+    | AVar name -> fprintf ppf "%s" name
+    | ALam (name, rhs) -> fprintf ppf "(fun %s -> %a)" name pp rhs
+    | CApp (l, r) -> fprintf ppf "(%a %a)" pp l pp r
+    | CAtom ea -> pp ppf ea
+    | CIte (cond, th, el) -> fprintf ppf "(if %a then %a else %a)" pp cond pp th pp el
+    | ELet (flg, name, rhs, wher) ->
+      fprintf
+        ppf
+        "@[<v>@[let %s%s = %a in@]@,@[%a@]@]"
+        (match flg with
+         | Recursive -> "rec "
+         | _ -> "")
+        name
+        pp
+        rhs
+        pp
+        wher
+    | EComplex ec -> pp ppf ec
+;;
+
+let gensym =
+  let n = ref 0 in
+  fun () ->
+    incr n;
+    !n
+;;
+
+open Monads.Cont
+open Monads.Cont.Syntax
+
+let to_any : _ expr -> any_expr = Obj.magic
+let complex_to_atom_comlex : [ `Complex ] expr -> [ `Complex | `Atom ] expr = Obj.magic
+let complex_to_any : [ `Complex | `Atom ] expr -> any_expr = Obj.magic
+
+let rec normalize : Typedtree.expr -> any_expr Monads.Cont.t =
+ fun m ->
   match m with
-  | Typedtree.TConst (PConst_int n) -> k (EAtom (AConst n))
-  | TLam (name, body, _) -> k (EAtom (ALam (name, normalize_term body)))
-  | TLet (flg, pat, _, body, wher) ->
-    normalize_c body (fun body ->
-      normalize wher (fun wher -> k (ELet (flg, pat, body, wher))))
-  (* Chaining below *)
-  | TIf _ | TApp _ -> normalize_c m (fun c -> k (EComplex c))
+  | Typedtree.TConst n -> return (EComplex (CAtom (AConst n)) |> to_any)
+  | TLam (name, body, _) ->
+    return (EComplex (CAtom (ALam (name, normalize_term body))) |> to_any)
+  | TLet (flg, pat, typ, body, wher) ->
+    (match body with
+     | TLet (flg2, pat2, typ2, body2, wher) ->
+       normalize (TLet (flg2, pat2, typ2, body2, TLet (flg, pat, typ, body, wher)))
+     | Typedtree.TLam (pat, expr, _) ->
+       let* anf_expr = normalize expr in
+       let* anf_wher = normalize wher in
+       return
+         (ELet
+            (flg, pat, CAtom (ALam ("arg", anf_expr)) |> complex_to_atom_comlex, anf_wher)
+         |> to_any)
+     | _ -> assert false)
+  | TIf (TLet (flg, name, rhs_typ, rhs, wher), eth, el, iftyp) ->
+    normalize (TLet (flg, name, rhs_typ, rhs, TIf (wher, eth, el, iftyp)))
+  | TIf (econd, eth, el, _) ->
+    let* anf_cond = normalize econd in
+    let rec hack (type a) : a expr -> _ = function
+      | ALam _ -> failwith "unreachable"
+      | AConst _ as anf_cond ->
+        let* anf_th = normalize eth in
+        let* anf_el = normalize el in
+        return (CIte (anf_cond, anf_th, anf_el) |> to_any)
+      | ELet (flg, name, rhs, wher) ->
+        let* inner = hack wher in
+        return (ELet (flg, name, rhs, inner))
+      | CApp (f, arg) ->
+        let* anf_th = normalize eth in
+        let* anf_el = normalize el in
+        let tempname = gensym () |> Printf.sprintf "cond%d" in
+        return
+          (ELet
+             ( NonRecursive
+             , tempname
+             , CApp (f, arg)
+             , EComplex (CIte (AVar tempname, anf_th, anf_el)) |> to_any ))
+      | anf ->
+        Format.kasprintf failwith "Not implemented; '%a' %s %d" pp anf __FILE__ __LINE__
+    in
+    hack anf_cond
+  | TApp (f, arg, _) ->
+    let rec wrap (type a) : ([ `Atom ] expr -> any_expr) -> a expr -> any_expr =
+     fun k -> function
+      | (AUnit as v)
+      | (AConst _ as v)
+      | (AVar _ as v)
+      | CAtom (AConst _ as v)
+      | EComplex (CAtom (AConst _ as v)) -> k v
+      | ALam _ as v -> k v
+      | EComplex (CAtom (ALam _ as v)) -> k v
+      | CAtom (ALam _ as v) -> k v
+      | CApp (l, r) ->
+        let tempname = gensym () |> Printf.sprintf "name%d" in
+        ELet (NonRecursive, tempname, CApp (l, r), k (AVar tempname)) |> to_any
+      | ELet (flg, name, rhs, wher) -> ELet (flg, name, rhs, wrap k wher) |> to_any
+      | anf ->
+        Format.kasprintf failwith "Not implemented; '%a' %s %d" pp anf __FILE__ __LINE__
+    in
+    let* anf_f = normalize f in
+    let* anf_arg = normalize arg in
+    wrap
+      (fun atomf ->
+        wrap (fun atom_arg -> CApp (atomf, atom_arg) |> complex_to_any) anf_arg)
+      anf_f
+    |> return
+  | TVar ("=", _) -> return (AVar "=" |> to_any)
+  | TVar (name, _) -> return (AVar name |> to_any)
   | _ -> Format.kasprintf failwith "Not implemented; '%a'" Pprinttyped.pp_hum m
 
-and normalize_c : 'a 'b. Typedtree.expr -> ([ `Complex ] expr -> 'a) -> 'a =
- fun m k ->
-  match m with
-  (*   | Typedtree.TApp (l, r, _) ->
-    normalize_a l (fun l -> normalize_a r (fun r -> k (CApp (l, r)))) *)
-  | TIf (cond, th, el, _ty) ->
-    normalize_a cond (fun cond ->
-      normalize th (fun th -> normalize el (fun el -> k (CIte (cond, th, el)))))
-  | TApp (l, r, _) ->
-    normalize_name l (fun l -> normalize_name r (fun r -> k (CApp (l, r))))
-
+(*
 and normalize_a : 'a 'b. Typedtree.expr -> ([ `Atom ] expr -> 'a) -> 'a =
  fun m k ->
   match m with
-  | Typedtree.TConst (PConst_int n) -> k (AConst n)
+  | Typedtree.TConst n -> k (AConst n)
+  | TUnit -> k AUnit
+  | TVar (name, _) -> k (AVar name)
   | _ ->
     Format.kasprintf failwith "Not implemented in 'normalize_a': %a" Pprinttyped.pp_hum m
-
-and normalize_name : 'a 'b. Typedtree.expr -> ([ `Atom ] expr -> 'a) -> 'a =
+ *)
+(* and normalize_name : 'a 'b. Typedtree.expr -> ([ `Atom ] expr -> 'a) -> 'a =
  fun m k ->
   match m with
-  | Typedtree.TConst (PConst_int n) -> k (AConst n)
+  | Typedtree.TConst n -> k (AConst n)
+ *)
+and normalize_term t = Monads.Cont.run_cont Fun.id (normalize t)
 
-and normalize_term t = normalize t Fun.id
-
-let%expect_test " " =
-  let text = {| let rec fac = fun n -> if n=0 then 1 else n * (fac (n-1))|} in
+let test_anf text =
   let ( let* ) x f = Result.bind x f in
-  (let stru = Miniml.Parsing.parse_vb_exn text in
-   let* { tvb_body = typed; _ } = Inferencer.vb stru in
-   let anf = normalize_term typed in
-   Format.printf "%a\n%!" pp anf;
-   Result.Ok ())
-  |> ignore;
-  [%expect.unreachable]
-  [@@expect.uncaught_exn
-    {|
-  (* CR expect_test_collector: This test expectation appears to contain a backtrace.
-     This is strongly discouraged as backtraces are fragile.
-     Please change this test to not include a backtrace. *)
+  match
+    let stru = Miniml.Parsing.parse_vb_exn text in
+    (* Format.printf "%s %d\n%!" __FILE__ __LINE__; *)
+    let* { tvb_body = typed; _ } = Inferencer.vb stru in
+    (* Format.printf "%s %d\n%!" __FILE__ __LINE__; *)
+    Result.ok (normalize_term typed)
+  with
+  | Result.Error err -> Format.printf "%a\n%!" Inferencer.pp_error err
+  | Ok anf -> Format.printf "%a\n%!" pp anf
+;;
 
-  (Failure "Not implemented in 'normalize_a': (n = 0)")
-  Raised at Stdlib.failwith in file "stdlib.ml", line 29, characters 17-33
-  Called from Compile_lib__ANF.normalize in file "compiler/ANF.ml", line 63, characters 50-69
-  Called from Compile_lib__ANF.(fun) in file "compiler/ANF.ml", line 101, characters 13-33
-  Called from Stdlib__Result.bind in file "result.ml" (inlined), line 23, characters 36-39
-  Called from Compile_lib__ANF.(fun).let* in file "compiler/ANF.ml", line 98, characters 21-36
-  Called from Expect_test_collector.Make.Instance_io.exec in file "collector/expect_test_collector.ml", line 262, characters 12-19 |}]
+let%expect_test "standard factorial" =
+  test_anf {| let rec fac = fun n -> if n=0 then 1 else n * (fac (n-1))|};
+  [%expect
+    {|
+    (fun n -> let name1 = (= n) in
+              let cond6 = (name1 0) in
+              (if cond6 then 1 else let name4 = (* n) in
+                                    let name2 = (- n) in
+                                    let name3 = (name2 1) in
+                                    let name5 = (fac name3) in
+                                    (name4 name5))) |}]
+;;
+
+let%expect_test "exponential fibonacci" =
+  test_anf
+    {| let rec fib = fun n ->
+        if n=0 then 0 else
+        if n=1 then 1 else
+        fib (n-1) + fib (n-2)|};
+  [%expect
+    {|
+    (fun n -> let name7 = (= n) in
+              let cond17 = (name7 0) in
+              (if cond17 then 0 else let name8 = (= n) in
+                                     let cond16 = (name8 1) in
+                                     (if cond16 then 1 else let name9 = (- n) in
+                                                            let name10 = (name9 1) in
+                                                            let name11 = (fib name10) in
+                                                            let name14 = (+ name11) in
+                                                            let name12 = (- n) in
+                                                            let name13 = (name12 2) in
+                                                            let name15 = (fib name13) in
+                                                            (name14 name15))))
+
+     |}]
+;;
+
+let%expect_test "CPS factorial" =
+  test_anf
+    {| let rec fack n k =
+    if n=0 then k 1
+    else fack (n-1) (fun p -> k (p*n)) |};
+  [%expect
+    {|
+    (fun n -> (fun k -> let name18 = (= n) in
+                        let cond24 = (name18 0) in
+                        (if cond24 then (k 1) else let name19 = (- n) in
+                                                   let name20 = (name19 1) in
+                                                   let name23 = (fack name20) in
+                                                   (name23 (fun p -> let name21 = (* p) in
+                                                                     let name22 = (name21 n) in
+                                                                     (k name22)))))) |}]
+;;
+
+let%expect_test "CPS fibonacci" =
+  test_anf
+    {| let rec fibk n k =
+        if n=0 then k 0 else
+        if n=1 then k 1 else
+        fibk (n-1) (fun p -> fibk (n-2) (fun q -> k(p+q))) |};
+  [%expect
+    {|
+    (fun n -> (fun k -> let name25 = (= n) in
+                        let cond36 = (name25 0) in
+                        (if cond36 then (k 0) else let name26 = (= n) in
+                                                   let cond35 = (name26 1) in
+                                                   (if cond35 then (k 1) else
+                                                   let name27 = (- n) in
+                                                   let name28 = (name27 1) in
+                                                   let name34 = (fibk name28) in
+                                                   (name34 (fun p -> let name29 = (- n) in
+                                                                     let name30 = (name29 2) in
+                                                                     let name33 = (fibk name30) in
+                                                                     (name33 (fun q ->
+                                                                     let name31 = (+ p) in
+                                                                     let name32 = (name31 q) in
+                                                                     (k name32)))))))))
+
+     |}]
 ;;
--- compiler/monads.ml
+++ compiler/monads.ml
@@ -1,8 +1,36 @@
-module Store = struct
+module type MONAD = sig
+  type 'a t
+
+  val return : 'a -> 'a t
+  val ( >>= ) : 'a t -> ('a -> 'b t) -> 'b t
+end
+
+module type MONAD_IDX1 = sig
+  type ('i, 'a) t
+
+  val return : 'a -> ('i, 'a) t
+  val ( >>= ) : ('i, 'a) t -> ('a -> ('i, 'b) t) -> ('i, 'b) t
+  val ( let* ) : ('i, 'a) t -> ('a -> ('i, 'b) t) -> ('i, 'b) t
+end
+
+module type MONADERROR = sig
+  include MONAD
+
+  val error : string -> 'a t
+end
+
+module Store : sig
+  include MONAD_IDX1
+
+  val get : ('acc, 'acc) t
+  val put : 'acc -> ('acc, unit) t
+  val ( <*> ) : 'acc 'a 'b. ('acc, 'a -> 'b) t -> ('acc, 'a) t -> ('acc, 'b) t
+  val run : ('acc, 'a) t -> 'acc -> 'acc * 'a
+end = struct
   type ('acc, 'a) t = 'acc -> 'acc * 'a
 
   let return : 'a 'acc. 'a -> ('acc, 'a) t = fun x acc -> acc, x
-  let get : ('acc, 'a) t = fun acc -> acc, acc
+  let get : ('acc, 'acc) t = fun acc -> acc, acc
   let put : 'acc -> ('acc, unit) t = fun next _ -> next, ()
   let run m init = m init
 
@@ -13,9 +41,37 @@ module Store = struct
     acc3, f arg
  ;;
 
-  let ( let* ) : 'acc 'a 'b. ('acc, 'a) t -> ('a -> ('acc, 'b) t) -> ('acc, 'b) t =
+  let ( >>= ) : 'acc 'a 'b. ('acc, 'a) t -> ('a -> ('acc, 'b) t) -> ('acc, 'b) t =
    fun x f st ->
     let st, rez = x st in
     f rez st
  ;;
+
+  let ( let* ) = ( >>= )
+end
+
+module Cont : sig
+  include MONADERROR
+
+  val run_cont : ('a -> 'b) -> 'a t -> 'b
+
+  module Syntax : sig
+    val ( let* ) : 'a t -> ('a -> 'b t) -> 'b t
+  end
+end = struct
+  type ('a, 'b) cont = 'a -> 'b
+  type 'a t = { cont : 'b. ('a, 'b) cont -> 'b }
+
+  let return (x : 'a) = { cont = (fun k -> k x) }
+
+  let ( >>= ) (x : 'a t) (f : 'a -> 'b t) : 'b t =
+    { cont = (fun k -> x.cont (fun v -> (f v).cont k)) }
+  ;;
+
+  let error = failwith
+  let run_cont f { cont } = cont f
+
+  module Syntax = struct
+    let ( let* ) = ( >>= )
+  end
 end
