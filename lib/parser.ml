6eeb33ba9161446fc9e2a2454cfc5b7786d0de00
Kakadu <Kakadu@pm.me>
2024-04-30 16:39:39 +0300

    Towards statements parser
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- lib/parser.ml
+++ lib/parser.ml
@@ -48,6 +48,20 @@ let char c =
     true)
   else false
 
+let lookahead_cond cond =
+  let curpos = ref !pos in
+  while !curpos < !length && cond !text.[!pos] do
+    incr curpos
+  done;
+  !curpos < !length && !text.[!curpos] = '('
+
+let lookahead_paren () =
+  let curpos = ref !pos in
+  while !curpos < !length && is_digit !text.[!pos] do
+    incr curpos
+  done;
+  !curpos < !length && !text.[!curpos] = '('
+
 let econst () =
   (* ws (); *)
   let acc = Buffer.create 5 in
@@ -59,35 +73,42 @@ let econst () =
     Option.some @@ EConst (int_of_string (Buffer.contents acc))
   else None
 
-let eident () =
-  (* ws (); *)
+let ident () =
+  ws ();
   let acc = Buffer.create 5 in
   log "eident: pos = %d" !pos;
   while !pos < !length && is_alpha !text.[!pos] do
     Buffer.add_char acc !text.[!pos];
     incr pos
   done;
+  if Buffer.length acc > 0 then Some (Buffer.contents acc) else None
 
-  if Buffer.length acc > 0 then Some (EVar (Buffer.contents acc)) else None
+let eident () = match ident () with Some x -> Some (EVar x) | None -> None
 
 let rec expr_plus () =
   let ch, op = ('+', "+") in
-  match primary () with
+  match expr_mul () with
   | None -> None
   | Some head ->
-      let acc = ref head in
-      let rec loop () =
-        let rb1 = Rollback.bmrk () in
-        if char ch then (
-          match expr_mul () with
-          | None -> Rollback.rollback rb1
-          | Some v ->
-              acc := EBinop (op, !acc, v);
-              loop ())
-        else Rollback.rollback rb1
-      in
-      loop ();
-      Some !acc
+      if lookahead_paren () then
+        let b1 : bool = char '(' in
+        let rez = expr_plus () in
+        let b2 : bool = char ')' in
+        match rez with Some x when b1 && b2 -> Some x | _ -> None
+      else
+        let acc = ref head in
+        let rec loop () =
+          let rb1 = Rollback.bmrk () in
+          if char ch then (
+            match expr_mul () with
+            | None -> Rollback.rollback rb1
+            | Some v ->
+                acc := EBinop (op, !acc, v);
+                loop ())
+          else Rollback.rollback rb1
+        in
+        let () = loop () in
+        Some !acc
 
 and expr_mul () =
   let ch, op = ('*', "*") in
@@ -109,18 +130,57 @@ and expr_mul () =
       Some !acc
 
 and primary () =
-  match eident () with
-  | Some x -> Some x
-  | None -> ( match econst () with None -> None | Some x -> Some x)
+  if lookahead_paren () then
+    let b1 : bool = char '(' in
+    let rez = expr_plus () in
+    let b2 : bool = char ')' in
+    if b1 && b2 then rez else None
+  else
+    match eident () with
+    | Some x ->
+        log "ident %S parsed in primary" ([%show: AST.expr] x);
+        Some x
+    | None -> ( match econst () with None -> None | Some x -> Some x)
 
 let expr = expr_plus
+
+let statement () =
+  match ident () with
+  | None -> None
+  | Some "if" -> assert false
+  | Some lhs -> (
+      let is_eq = char '=' in
+      let rhs = expr () in
+      log "%s %d %s" __FILE__ __LINE__ @@ [%show: AST.expr option] rhs;
+
+      let is_semi = char ';' in
+      match rhs with
+      | Some r when is_eq && is_semi -> Some (Assgn (lhs, r))
+      | _ -> None)
 
 let parse_expr_string s =
   init s;
   expr ()
 
 let parse_print_expr str =
-  Format.printf "%s\n%!" @@ [%show: AST.expr option] (parse_expr_string str)
+  match parse_expr_string str with
+  | Some ast -> Format.printf "%s\n%!" @@ [%show: AST.expr] ast
+  | None -> print_endline "ERROR"
+
+let program () =
+  let rec loop acc =
+    match statement () with None -> List.rev acc | Some v -> loop (v :: acc)
+  in
+  let stmts = loop [] in
+  ws ();
+  Some stmts
+(* if !pos >= !length then Some stmts else None *)
+
+let parse_print_program str =
+  init str;
+  match program () with
+  | Some ast -> Format.printf "%s\n%!" @@ [%show: AST.stmt list] ast
+  | None -> print_endline "ERROR"
 
 let parse ~filename =
   text := In_channel.with_open_text filename In_channel.input_all;
@@ -133,24 +193,76 @@ let%expect_test "just ident" =
   [%expect {|
     (Some (EVar "a")) |}]
 
+let%expect_test "just a constant" =
+  logon ();
+  parse_print_expr "42";
+  [%expect {|
+    eident: pos = 0
+    (EConst 42) |}]
+
 let%expect_test "a+b+c" =
   logoff ();
   parse_print_expr "a+b+c";
   [%expect
     {|
-        (Some (EBinop ("+", (EBinop ("+", (EVar "a"), (EVar "b"))), (EVar "c")))) |}]
+        (EBinop ("+", (EBinop ("+", (EVar "a"), (EVar "b"))), (EVar "c"))) |}]
 
 let%expect_test "a+b*c" =
   logoff ();
   parse_print_expr "a+b*c";
   [%expect
     {|
-    (Some (EBinop ("+", (EVar "a"), (EBinop ("*", (EVar "b"), (EVar "c")))))) |}]
+    (EBinop ("+", (EVar "a"), (EBinop ("*", (EVar "b"), (EVar "c"))))) |}]
 
-let%expect_test "a+b*c" =
+let%expect_test "b*c+a" =
+  logoff ();
+  parse_print_expr "b*c+a";
+  [%expect
+    {|
+    (EBinop ("+", (EBinop ("*", (EVar "b"), (EVar "c"))), (EVar "a"))) |}]
+
+let%expect_test "a+b*c+1" =
   logoff ();
   parse_print_expr "a+b*c+1";
-  [%expect{|
-    (Some (EBinop ("+",
-             (EBinop ("+", (EVar "a"), (EBinop ("*", (EVar "b"), (EVar "c"))))),
-             (EConst 1)))) |}]
+  [%expect
+    {|
+    (EBinop ("+",
+       (EBinop ("+", (EVar "a"), (EBinop ("*", (EVar "b"), (EVar "c"))))),
+       (EConst 1))) |}]
+
+let%expect_test "parens: (b)" =
+  parse_print_expr "(b)";
+  [%expect {|
+    (EVar "b") |}]
+
+let%expect_test "parens " =
+  parse_print_expr "(b)+1";
+  [%expect {|
+    (EBinop ("+", (EVar "b"), (EConst 1))) |}]
+
+let%expect_test "parens " =
+  parse_print_expr "(b+a)*c";
+  [%expect
+    {|
+    (EBinop ("*", (EBinop ("+", (EVar "b"), (EVar "a"))), (EVar "c"))) |}]
+
+let%expect_test "program" =
+  parse_print_program "x=5;";
+  [%expect {|
+    [(Assgn ("x", (EConst 5)))] |}]
+
+let%expect_test "program" =
+  parse_print_program "if (a) then x=5; fi";
+  [%expect.unreachable]
+[@@expect.uncaught_exn {|
+  (* CR expect_test_collector: This test expectation appears to contain a backtrace.
+     This is strongly discouraged as backtraces are fragile.
+     Please change this test to not include a backtrace. *)
+
+  "Assert_failure lib/parser.ml:150:17"
+  Raised at Parser.statement in file "lib/parser.ml", line 150, characters 17-29
+  Called from Parser.program.loop in file "lib/parser.ml", line 172, characters 10-22
+  Called from Parser.program in file "lib/parser.ml", line 174, characters 14-21
+  Called from Parser.parse_print_program in file "lib/parser.ml", line 181, characters 8-18
+  Called from Parser.(fun) in file "lib/parser.ml", line 255, characters 2-43
+  Called from Expect_test_collector.Make.Instance_io.exec in file "collector/expect_test_collector.ml", line 262, characters 12-19 |}]
