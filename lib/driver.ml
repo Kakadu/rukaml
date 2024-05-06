b9b3a1817fb5269aa91ee2af7c3ec179a115de6b
Kakadu <Kakadu@pm.me>
2024-05-06 20:49:14 +0300

    WIP
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- lib/driver.ml
+++ lib/driver.ml
@@ -2,7 +2,7 @@ type cmd_args = { mutable file : string }
 
 let args = { file = "" }
 let fprintf = Format.fprintf
-let printfn ppf fmt = Format.kfprintf (fun ppf -> fprintf ppf "\n") ppf fmt
+(* let printfn ppf fmt = Format.kfprintf (fun ppf -> fprintf ppf "\n") ppf fmt *)
 
 let () =
   Arg.parse [] (fun s -> args.file <- s) "help";
@@ -14,12 +14,45 @@ let () =
           let ppf = Format.formatter_of_out_channel outch in
           let locals = AST.all_vars p in
 
-          printfn ppf ".global _start\n";
-          printfn ppf "_start:\n";
+          let printfn fmt =
+            Format.kfprintf (fun ppf -> fprintf ppf "\n") ppf fmt
+          in
+
+          printfn ".section .data";
+          printfn ".equ BUFSIZE,32";
+          (* printfn
+             "the_buffer: .string \"01234567012345670123456701234567\\n\" # \
+              32+1 bytes"; *)
+          locals
+          |> AST.String_set.iter (fun s ->
+                 printfn "varname_%s: .string  %S" s s;
+                 printfn ".equ VARLEN_%s, %d" s (String.length s));
+
+          printfn ".text";
+          printfn ".extern memset";
+          printfn ".global _start";
+          printfn "_start:";
           (* printfn ppf "  addi sp, sp, %+d*8" (AST.String_set.cardinal locals); *)
           Codegen.codegen (List.of_seq (AST.String_set.to_seq locals), p) ppf;
-          printfn ppf "  li a0, 22";
-          printfn ppf "  li a7, 93";
-          printfn ppf "  ecall\n";
+
+          (* Code to print everything *)
+          locals
+          |> AST.String_set.iter (fun s ->
+                 printfn "# trace variable %S" s;
+                 printfn "la a0, str2";
+                 printfn "li a1, 0x20 # space";
+                 printfn "li a2, BUFSIZE+1";
+                 printfn "call memset";
+                 printfn "li a0, 123";
+                 printfn "call myitoa";
+                 printfn "li      a7, 64  # write on RISCV linux";
+                 printfn "li      a0, 1";
+                 printfn "la      a1, str2";
+                 printfn "li      a2, 33";
+                 printfn "ecall");
+
+          printfn "  li a0, 22";
+          printfn "  li a7, 93";
+          printfn "  ecall\n";
 
           Format.pp_print_flush ppf ())
--- test/run.t
+++ test/run.t
@@ -1,11 +1,14 @@
+$ cat > asdf.pas <<-EOF
+> x=6;
+> acc=1;
+> while x>0 do acc=acc*x; x=x-1; done
+> EOF
   $ cat > asdf.pas <<-EOF
   > x=6;
-  > acc=1;
-  > while x>0 do acc=acc*x; x=x-1; done
   > EOF
   $ cat asdf.pas
   $ ../lib/driver.exe asdf.pas
   $ cat out.s
-  $ riscv64-linux-gnu-as -march=rv64imac out.s -o hello.o
-  $ riscv64-linux-gnu-ld hello.o -o program.exe
+  $ riscv64-linux-gnu-as -march=rv64gc_zbb out.s -o hello.o
+  $ riscv64-linux-gnu-ld hello.o rv64_runtime.o -o program.exe
   $ qemu-riscv64 -L /usr/riscv64-linux-gnu -cpu rv64 ./program.exe
