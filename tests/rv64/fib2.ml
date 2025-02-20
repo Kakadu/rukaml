c5156d54271f8e60e24a2e0320b132acadebe0f3
Kakadu <Kakadu@pm.me>
2025-02-20 20:47:01 +0300

    Fixup tests
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- tests/rv64/fib2.ml
+++ tests/rv64/fib2.ml
@@ -1,7 +1,21 @@
-let return x = x
-let main = 
-  let rec fib n k1 = 
-    if n < 2 then k1 n 
-    else fib (n - 1) (fun t2 ->  fib (n - 2) (fun t3 -> k1 (t2 + t3))) 
-  in 
-  fib 6 (fun t4 -> let g = print t4 in return 0)
+let id x = x
+
+let k2 k1 t3 =
+  let w = print 102 in
+  k1 t3
+
+let k3 fib n k t2 =
+  let z = k2 k in
+  let w = print 52 in
+  fib (n - 2) z
+
+let rec fib n k =
+  if n < 2 then k n
+  else
+    let z = k3 fib n k in
+    fib (n - 1) z
+
+let main =
+  let w = fib 2 id in
+  let z = print w in
+  0
