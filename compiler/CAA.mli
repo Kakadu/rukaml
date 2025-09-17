fd3527f86ccfec9ce0b4b369be1029c1349142d9
Nikita Shchutskii <nshchutskiy@gmail.com>
2025-09-17 15:57:05 +0300

    feat/fix: conditional dead code elem disabling (by default dead code elem is disabled for now)

--- compiler/CAA.mli
+++ compiler/CAA.mli
@@ -1,4 +1,4 @@
 open CPSLang
 
-val call_arity_anal : OneACPS.cps_vb -> MACPS.cps_vb
-val call_arity_anal_debug : OneACPS.cps_vb -> MACPS.cps_vb
+val call_arity_anal : ?disable_dead_code_elem:bool -> OneACPS.cps_vb -> MACPS.cps_vb
+val call_arity_anal_debug : ?disable_dead_code_elem:bool -> OneACPS.cps_vb -> MACPS.cps_vb
