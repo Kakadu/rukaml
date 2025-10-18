a717fa79b59e34e635c113e38ac81584ff44f8b0
Andrei <therain.i@yahoo.com>
2025-10-19 01:29:55 +0300

    test: convert anf `large` test

---
+++ testsuite/tests/fail/large.ml
@@ -0,0 +1,15 @@
+(*
+test
+  (targets (amd64 promote))
+  (run (stdout "rukaml_print_int 0"))
+*)
+
+(* should print 42 then 0 *)
+
+let large x = if x then print 0 else print 1
+let main =
+  let x = if (if (if false
+                  then false else (let t42 = print 42 in true))
+              then false else true)
+          then false else true in
+  large x
