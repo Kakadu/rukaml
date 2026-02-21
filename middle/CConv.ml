37be770a46cb06e8ef5b2970008667522ab01183
prettysorrow <artyombaly@list.ru>
2026-02-22 02:03:52 +0300

    fix minor bug in cconv
    Signed-off-by: prettysorrow <artyombaly@list.ru>

--- middle/CConv.ml
+++ middle/CConv.ml
@@ -339,15 +339,17 @@ let conv ?(standart_globals = standart_globals)
       return (elet ~isrec pat rhs body)
     | ELet (_, PConstruct _, _, _) | ELet (_, PConst _, _, _) ->
       failwith "not implemented 3"
-  and helper_list globals : Parsetree.expr list -> (value_binding list, expr list) t =
-    fun es ->
-    List.fold_left
-      (fun acc e ->
-         let* acc = acc in
-         let* e = helper globals e in
-         return (e :: acc))
-      (return [])
-      es
+  and helper_list globals es =
+    let* es =
+      List.fold_left
+        (fun acc e ->
+           let* acc = acc in
+           let* e = helper globals e in
+           return (e :: acc))
+        (return [])
+        es
+    in
+    return (List.rev es)
   in
   function
   | is_rec, (PVar v as pat), root ->
