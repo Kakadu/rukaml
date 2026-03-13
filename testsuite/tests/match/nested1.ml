030e48433361c00584d2ebe8fd1ad38873f38c2e
prettysorrow <artyombaly@list.ru>
2026-03-13 22:47:08 +0300

    move tests for adt and match-with to separated directory
    Signed-off-by: prettysorrow <artyombaly@list.ru>

---
+++ testsuite/tests/match/nested1.ml
@@ -0,0 +1,21 @@
+(*
+   test
+  (targets amd64)
+  (run (stdout "test passed"))
+*)
+
+let test () =
+  match [ [ 0; 1 ]; [ 2 ] ] with
+  | [] -> printf "test failed 1"
+  | [ _ ] -> printf "test failed 2"
+  | [] :: _ -> printf "test failed 3"
+  | [ _ ] :: _ -> printf "test failed 4"
+  | [ _; _ ] :: [] :: _ -> printf "test failed 5"
+  | [ [ _; _ ]; [ _ ] ] -> printf "test passed"
+  | _ -> printf "test failed 6"
+;;
+
+let main =
+  let t = test () in
+  0
+;;
