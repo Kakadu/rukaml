6462aaa8bff0df0ac478043afeb87b022d9c6b50
prettysorrow <artyombaly@list.ru>
2026-03-28 23:57:45 +0300

    add weird lambda example (todo: revert this commit)
    Signed-off-by: prettysorrow <artyombaly@list.ru>

---
+++ testsuite/tests/parsing/lambda.ml
@@ -0,0 +1,221 @@
+(*
+   test
+  (targets amd64)
+  (run (stdout "rukaml_print_int 20"))
+*)
+
+(* TODO: remove this example before merge *)
+
+(* [begin skip] *)
+
+let string_of_char_list chs = String.of_seq (List.to_seq chs)
+let string_len s = String.length s
+let string_nth s n = s.[n]
+let char_code = Char.code
+let printf, fprintf, sprintf = Stdlib.Printf.(printf, fprintf, sprintf)
+
+(* [end skip] *)
+
+type parsing_error =
+  | Perr_eof
+  | Perr_match of int
+  | Perr_unexpected_char of char
+
+let pp_parsing_error oc err =
+  match err with
+  | Perr_eof -> fprintf oc "unexpected eof"
+  | Perr_match n -> fprintf oc "match failed %d" n
+  | Perr_unexpected_char ch -> fprintf oc "unexpected char: %c" ch
+;;
+
+type 'a parsing_result =
+  | Prez_ok of ('a * char list)
+  | Prez_err of parsing_error
+
+type 'a parser = char list -> 'a parsing_result
+type 'a fix = Fix of ('a fix -> 'a)
+
+let fix f =
+  let rec g x =
+    match x with
+    | Fix x -> f (fun a -> g (Fix x) a)
+  in
+  g (Fix g)
+;;
+
+let rec skip_ws chs =
+  match chs with
+  | [] -> Prez_ok ((), [])
+  | ch :: chs ->
+    if char_code ch = char_code ' ' then skip_ws chs else Prez_ok ((), ch :: chs)
+;;
+
+let parse_var chs =
+  match skip_ws chs with
+  | Prez_err err -> Prez_err err
+  | Prez_ok ((), chs) ->
+    (match chs with
+     | [] -> Prez_err Perr_eof
+     | ch :: chs ->
+       if char_code 'a' <= char_code ch && char_code ch <= char_code 'z'
+       then Prez_ok (ch, chs)
+       else Prez_err (Perr_unexpected_char ch))
+;;
+
+let skip_char ch chs =
+  match skip_ws chs with
+  | Prez_err err -> Prez_err err
+  | Prez_ok ((), hd :: tl) ->
+    if ch = hd then Prez_ok ((), tl) else Prez_err (Perr_unexpected_char hd)
+  | _ -> Prez_err Perr_eof
+;;
+
+let parens parse chs =
+  match skip_char '(' chs with
+  | Prez_err err -> Prez_err err
+  | Prez_ok ((), chs) ->
+    (match parse chs with
+     | Prez_err err -> Prez_err err
+     | Prez_ok (expr, chs) ->
+       (match skip_char ')' chs with
+        | Prez_err err -> Prez_err err
+        | Prez_ok ((), chs) -> Prez_ok (expr, chs))
+     | _ -> Prez_err (Perr_match 2))
+;;
+
+let string_to_char_list s =
+  let len = string_len s in
+  let rec helper n = if n >= len then [] else string_nth s n :: helper (n + 1) in
+  helper 0
+;;
+
+let test0 =
+  match parse_var (string_to_char_list " x ") with
+  | Prez_ok (ch, tl) -> printf "(0) parsed var: %c\n" ch
+  | Prez_err err -> printf "(0) parsing error: %a" pp_parsing_error err
+;;
+
+let test1 =
+  match parens parse_var (string_to_char_list "(x)") with
+  | Prez_ok (ch, tl) -> printf "(1) parsed var: %c\n" ch
+  | Prez_err err -> printf "(1) parsing error: %a" pp_parsing_error err
+;;
+
+let test2 =
+  match parens parse_var (string_to_char_list " ( x ) ") with
+  | Prez_ok (ch, tl) -> printf "(2) parsed var: %c\n" ch
+  | Prez_err err -> printf "(2) parsing error: %a\n" pp_parsing_error err
+;;
+
+type expr =
+  | Var of char
+  | Abs of (char * expr)
+  | App of (expr * expr)
+
+let rec pp_expr parens oc expr =
+  match expr with
+  | Var name -> fprintf oc "%c" name
+  | Abs (name, e) ->
+    fprintf oc (if parens then "(> %c . %a)" else "> %c . %a") name (pp_expr false) e
+  | App (e1, e2) ->
+    fprintf oc (if parens then "(%a %a)" else "%a %a") (pp_expr true) e1 (pp_expr true) e2
+;;
+
+let parse_atom parse_expr =
+  fix (fun self chs ->
+    match parens parse_expr chs with
+    | Prez_ok ok -> Prez_ok ok
+    | _ ->
+      (match parse_var chs with
+       | Prez_err err -> Prez_err err
+       | Prez_ok (name, chs) -> Prez_ok (Var name, chs)))
+;;
+
+let parse_evar chs =
+  match parse_var chs with
+  | Prez_err err -> Prez_err err
+  | Prez_ok (name, tl) -> Prez_ok (Var name, tl)
+;;
+
+let test3 =
+  match parse_atom parse_evar (string_to_char_list " ( x ) ") with
+  | Prez_ok (e, tl) -> printf "(3) parsed expr: %a\n" (pp_expr true) e
+  | Prez_err err -> printf "(3) parsing error: %a\n" pp_parsing_error err
+;;
+
+let parse_app parse_expr =
+  fix (fun self chs ->
+    match parse_expr chs with
+    | Prez_err err -> Prez_err err
+    | Prez_ok (left, chs) ->
+      (match skip_ws chs with
+       | Prez_err err -> Prez_err err
+       | Prez_ok ((), chs) ->
+         (match self chs with
+          | Prez_ok (right, chs) -> Prez_ok (App (left, right), chs)
+          | Prez_err _ ->
+            (match parse_expr chs with
+             | Prez_err _ -> Prez_ok (left, chs)
+             | Prez_ok (right, chs) -> Prez_ok (App (left, right), chs))))
+    | _ -> Prez_err (Perr_match 42))
+;;
+
+let test4 =
+  match parse_app (parse_atom parse_evar) (string_to_char_list " ( x ) y ") with
+  | Prez_ok (e, tl) -> printf "(4) parsed expr: %a\n" (pp_expr true) e
+  | Prez_err err -> printf "(4) parsing error: %a\n" pp_parsing_error err
+;;
+
+let parse_abs parse_expr =
+  fix (fun self chs ->
+    match skip_char '>' chs with
+    | Prez_err err -> Prez_err err
+    | Prez_ok ((), chs) ->
+      (match parse_var chs with
+       | Prez_err err -> Prez_err err
+       | Prez_ok (var, chs) ->
+         (match skip_char '.' chs with
+          | Prez_err err -> Prez_err err
+          | Prez_ok ((), chs) ->
+            (match self chs with
+             | Prez_err err ->
+               (match parse_expr chs with
+                | Prez_err err -> Prez_err err
+                | Prez_ok (body, tl) -> Prez_ok (Abs (var, body), tl))
+             | Prez_ok (body, chs) -> Prez_ok (Abs (var, body), chs)))))
+;;
+
+let test5 =
+  match
+    parse_abs (parse_app (parse_atom parse_evar)) (string_to_char_list "> x . ( x ) y ")
+  with
+  | Prez_ok (e, tl) -> printf "(5) parsed expr: %a\n" (pp_expr true) e
+  | Prez_err err -> printf "(5) parsing error: %a\n" pp_parsing_error err
+;;
+
+let test6 =
+  match
+    parse_abs
+      (parse_app (parse_atom parse_evar))
+      (string_to_char_list "> f . > x . > y . f x y ")
+  with
+  | Prez_ok (e, tl) -> printf "(6) parsed expr: %a\n" (pp_expr true) e
+  | Prez_err err -> printf "(6) parsing error: %a\n" pp_parsing_error err
+;;
+
+(* adding the declaration of parse_expr (even without calling it) breaks something *)
+
+(* let parse_expr chs =
+  fix (fun self chs ->
+    let parse_atom = parse_atom self in
+    let parse_app = parse_app parse_atom in
+    let parse_abs = parse_abs parse_app in
+    match parse_abs chs with
+    | Prez_ok ok -> Prez_ok ok
+    | _ ->
+      (match parse_app chs with
+       | Prez_ok ok -> Prez_ok ok
+       | _ -> parse_atom chs))
+;; *)
+
+let main = 0
