21e841d436013e008bfb90daa2a695a9df361453
prettysorrow <artyombaly@list.ru>
2026-04-03 18:34:23 +0300

    promote ppx_expect tests
    Signed-off-by: prettysorrow <artyombaly@list.ru>

--- middle/ANF_tests.ml
+++ middle/ANF_tests.ml
@@ -26,30 +26,30 @@ let%expect_test _ =
   test_anf_pat "(x,y)";
   [%expect
     {|
-    (fun temp1 -> let x = field 0 temp1 in
-                    let y = field 1 temp1 in
-                      use_pattern_vars_here) |}]
+    (fun tuple1 -> let x = block_nth tuple1 0 in
+                     let y = block_nth tuple1 1 in
+                       use_pattern_vars_here) |}]
 ;;
 
 let%expect_test _ =
   test_anf_pat "(x,y,z)";
   [%expect
     {|
-    (fun temp1 -> let x = field 0 temp1 in
-                    let y = field 1 temp1 in
-                      let z = field 2 temp1 in
-                        use_pattern_vars_here) |}]
+    (fun tuple1 -> let x = block_nth tuple1 0 in
+                     let y = block_nth tuple1 1 in
+                       let z = block_nth tuple1 2 in
+                         use_pattern_vars_here) |}]
 ;;
 
 let%expect_test _ =
   test_anf_pat "((x,y),z)";
   [%expect
     {|
-    (fun temp1 -> let temp2 = field 0 temp1 in
-                    let x = field 0 temp2 in
-                      let y = field 1 temp2 in
-                        let z = field 1 temp1 in
-                          use_pattern_vars_here) |}]
+    (fun tuple1 -> let field2 = block_nth tuple1 0 in
+                     let x = block_nth field2 0 in
+                       let y = block_nth field2 1 in
+                         let z = block_nth tuple1 1 in
+                           use_pattern_vars_here) |}]
 ;;
 
 let test_anf ?(print_before = false) text =
@@ -79,14 +79,14 @@ let%expect_test "CPS factorial" =
     else fack (n-1) (fun p -> k (p*n)) |};
   [%expect
     {|
-    let fresh_1 n k p =
+    let __lifted_lam_1 n k p =
       let temp1 = (p * n) in
         k temp1
     let rec fack n k =
       (if (n = 0)
       then k 1
       else let temp5 = (n - 1) in
-             let temp8 = fresh_1 n k in
+             let temp8 = __lifted_lam_1 n k in
                fack temp5 temp8)
     |}]
 ;;
@@ -105,11 +105,11 @@ let%expect_test _ =
   test_anf {| let foo = ((fun x -> x), (fun y -> y)) |};
   [%expect
     {|
-    let fresh_2 x =
+    let __lifted_lam_2 x =
       x
-    let fresh_3 y =
+    let __lifted_lam_3 y =
       y
     let foo =
-      (fresh_2, fresh_3)
+      (__lifted_lam_2, __lifted_lam_3)
     |}]
 ;;
--- middle/CPSConv.ml
+++ middle/CPSConv.ml
@@ -252,19 +252,19 @@ let%expect_test "counts simple" =
   test_count {| let m x y z = x y y|};
   [%expect
     {|
-    var x got id 65
-    var y got id 66
-    var z got id 67
-    var m got id 68
-    id: 65; counts 1
-    id: 66; counts 2
-    id: 67; counts 0
-    id: 68; counts 0
+    var x got id 71
+    var y got id 72
+    var z got id 73
+    var m got id 74
+    id: 71; counts 1
+    id: 72; counts 2
+    id: 73; counts 0
+    id: 74; counts 0
     ids that ref_once:
-    65
+    71
     ids that no_refs:
-    67
-    68
+    73
+    74
     |}]
 ;;
 
@@ -272,22 +272,22 @@ let%expect_test "counts branching, shadowing" =
   test_count {| let m x y = if x then fun x -> x 1 else fun x -> (y , y x)|};
   [%expect
     {|
-    var x got id 69
-    var y got id 70
-    var x got id 71
-    var x got id 72
-    var m got id 73
-    id: 69; counts 1
-    id: 70; counts 2
-    id: 71; counts 1
-    id: 72; counts 1
-    id: 73; counts 0
+    var x got id 75
+    var y got id 76
+    var x got id 77
+    var x got id 78
+    var m got id 79
+    id: 75; counts 1
+    id: 76; counts 2
+    id: 77; counts 1
+    id: 78; counts 1
+    id: 79; counts 0
     ids that ref_once:
-    69
-    71
-    72
+    75
+    77
+    78
     ids that no_refs:
-    73
+    79
     |}]
 ;;
 
@@ -295,12 +295,12 @@ let%expect_test "counts rec, ptuple" =
   test_count {| let rec (x,y) = x x y|};
   [%expect
     {|
-    var x got id 74
-    var y got id 75
-    id: 74; counts 2
-    id: 75; counts 1
+    var x got id 80
+    var y got id 81
+    id: 80; counts 2
+    id: 81; counts 1
     ids that ref_once:
-    75
+    81
     ids that no_refs:
     |}]
 ;;
