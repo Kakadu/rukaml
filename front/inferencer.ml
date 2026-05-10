344e909e19faffe9f5a1e8ead7e19eb5d8c6cce5
Kakadu <Kakadu@pm.me>
2026-05-11 00:37:03 +0300

    Make array builtins a proper builtins
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- front/inferencer.ml
+++ front/inferencer.ml
@@ -979,11 +979,13 @@ let start_env =
        (Typedtree.S (Var_set.singleton 0, tarrow (array_typ (tv 0 ~level:1000)) int_typ))
   |> extend_s
        "array_get"
+       ~kind:(Builtin ("array_get", 2))
        (let param = tv 0 ~level:1000 in
         let ( @-> ) = tarrow in
         Typedtree.S (Var_set.singleton 0, array_typ param @-> int_typ @-> param))
   |> extend_s
        "array_set"
+       ~kind:(Builtin ("array_set", 3))
        (let param = tv 0 ~level:1000 in
         Typedtree.S
           (Var_set.singleton 0, array_typ param @-> int_typ @-> param @-> unit_typ))
