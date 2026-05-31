776d9a4bb16108dd6f2f6136af5343e1096874ba
prettysorrow <artyombaly@list.ru>
2026-05-31 19:20:37 +0300

    delete deprecated test for arrays
    Signed-off-by: prettysorrow <artyombaly@list.ru>

--- testsuite/tests/array/array4.ml
+++
@@ -1,9 +0,0 @@
-(*
-test
-  (targets (amd64 promote) (rv64 promote))
-  (run (stdout "rukaml_print_int 5") (sh "echo test > foo"))
-*)
-
-let main =
-  let file = open_in "foo" in
-  print (length file)
