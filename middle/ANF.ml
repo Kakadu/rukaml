cc8c0701affd1973114b2833052095c0e46b7b68
prettysorrow <artyombaly@list.ru>
2026-05-31 19:21:56 +0300

    revert lowered constructors optimization (it would be added back later)
    Signed-off-by: prettysorrow <artyombaly@list.ru>

--- middle/ANF.ml
+++ middle/ANF.ml
@@ -616,7 +616,10 @@ let anf =
         __LINE__
         Pprinttyped.pp_hum
         m
-    | TConstruct (ident, [], _) -> k (AConstruct (ident.id, []))
+    | TConstruct (ident, [], _) ->
+      let name = gensym_id () in
+      let rhs = CAtom (AConstruct (ident.id, [])) in
+      make_let_nonrec name rhs (k (AVar name))
     | TConstruct (ident, args, _) ->
       let rec aux = function
         | hd :: tl, acc -> helper hd (fun x -> aux (tl, x :: acc))
