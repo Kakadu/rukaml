1d10c06ba322ff45e6e6564f5d5692d44075baf7
prettysorrow <artyombaly@list.ru>
2026-06-02 16:27:21 +0300

    fix casting and include warnings in rukaml_stdlib.c
    Signed-off-by: prettysorrow <artyombaly@list.ru>

--- back/llvm/rukaml_stdlib.c
+++ back/llvm/rukaml_stdlib.c
@@ -5,7 +5,6 @@
 #include <stdarg.h>
 #include <assert.h>
 #include <stdbool.h>
-#include <errno.h>
 
 /* #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 #define log_error(M, ...) fprintf(stderr, "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
