cbac3d6f1fe711107c5223f91f338b49a8b267a7
prettysorrow <artyombaly@list.ru>
2026-06-02 19:07:15 +0300

    add tests for "="
    Signed-off-by: prettysorrow <artyombaly@list.ru>

---
+++ testsuite/tests/equal/tuple1.ml
@@ -0,0 +1,14 @@
+(*
+   test
+  (targets amd64)
+  (run
+    (stdout
+      "(1) test passed"
+      "(2) test passed"))
+*)
+
+let test b n = printf (if b then "(%d) test passed\n" else "(%d) test failed\n") n
+let not b = if b then false else true
+let test1 = test ((1, 2, 3) = (0 + 1, 1 + 1, 3 * 1)) 1
+let test2 = test ((true, [ 1 ]) = ((if true then true else false), 1 :: [])) 2
+let main = 0
