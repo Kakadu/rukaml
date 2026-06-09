272358d77b97c472e0da381ee8b2d912c1f8bb2a
Kakadu <Kakadu@pm.me>
2026-06-09 14:27:35 +0300

    Fix test: Don't print real addresses
    In the test we used trace_rukaml_val for debug purposes.
    It prints real addresses of ponters, so the test is flacky.
    Let's not do that.
    
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- testsuite/tests/match_nested.ml
+++ testsuite/tests/match_nested.ml
@@ -2,23 +2,6 @@
 test
   (targets (rv64 promote))
   (run (stdout
-  "BLOCK: 0x152a8, tag=0, size=1"
-  " 0 -> Int 0"
-  "BLOCK: 0x152d8, tag=1, size=2"
-  " BLOCK: 0x152f0, tag=1, size=0"
-  " BLOCK: 0x152b8, tag=1, size=2"
-  "  BLOCK: 0x152a8, tag=0, size=1"
-  "   0 -> Int 0"
-  "  BLOCK: 0x152d0, tag=0, size=0"
-  "BLOCK: 0x15308, tag=1, size=2"
-  " BLOCK: 0x152f8, tag=0, size=1"
-  "  BLOCK: 0x152d8, tag=1, size=2"
-  "   BLOCK: 0x152f0, tag=1, size=0"
-  "   BLOCK: 0x152b8, tag=1, size=2"
-  "    BLOCK: 0x152a8, tag=0, size=1"
-  "     0 -> Int 0"
-  "    BLOCK: 0x152d0, tag=0, size=0"
-  " BLOCK: 0x15320, tag=0, size=0"
   "rukaml_print_int 3"
   ))
 *)
@@ -29,11 +12,11 @@ type 'a option =
 
 let main =
   let s0 = Some 0 in
-  let u = trace_rukaml_val s0 in
+  (* let u = trace_rukaml_val s0 in *)
   let s1 = [ None; s0 ] in
-  let u = trace_rukaml_val s1  in
+  (* let u = trace_rukaml_val s1  in *)
   let s2 = [ Some s1 ] in
-  let u = trace_rukaml_val s2 in
+  (* let u = trace_rukaml_val s2 in *)
   match s2 with
   | [] -> print 0
   | x :: xs ->
