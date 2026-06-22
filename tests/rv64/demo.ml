e4096b3744cea017ba12fe304f66e933e8870dda
Kakadu <Kakadu@pm.me>
2026-06-22 22:08:31 +0300

    Commit flawed test into tests/rv64. Need to debug double execution
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- tests/rv64/demo.ml
+++ tests/rv64/demo.ml
@@ -1,23 +1,20 @@
+(*
+   test
+  (targets amd64 rv64)
+  (run
+    (stdout
+      "(1) test passed"
+      "(2) test passed"
+      "(3) test passed"))
+*)
 
-let test () =
-  let scru = (1, "one") in
-  let (temp8, temp9) = scru in
-  (if (temp9 = "two")
-    then output_string stdout "equal"
-    else output_string stdout "not equal")
-  (* match scru with
-  (* | _, 0, _, _ -> printf "test failed 1"
-  | _, _, false, _ -> printf "test failed 2" *)
-  | _,   "two" -> output_string stdout "test failed 3"
-  (* | (), 1, true, "one" -> printf "test passed"
-  | (), 1, true, _ -> printf "test failed 4"
-  | (), 1, _, _ -> printf "test failed 5"
-  | (), _, _, _ -> printf "test failed 6"
-  | _, _, _, _ -> printf "test failed 7" *)
-  | _ -> output_string stdout "test failed 8" *)
-;;
-
-let main =
-  let t = test () in
-  0
-;;
+let test b n = printf (if b then "(%d) test passed\n" else "(%d) test failed\n") n
+let not b = if b then false else true
+let array1 = [| 0; 1; 2; 3; 4; 5 |]
+let array2 = [| 0; 1; 2; 3; 4; 5 |]
+let test1 = test (array1 = array2) 1
+let () = array_set array1 1 42
+let test2 = test (not (array1 = array2)) 2
+(* let () = array_set array1 1 1 *)
+(* let test3 = test (array1 = array2) 3 *)
+let main = 0
