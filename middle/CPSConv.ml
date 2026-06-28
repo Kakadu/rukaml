55cf95b046fe8ed868d946d9070fc87168325c6f
Kakadu <Kakadu@pm.me>
2026-06-28 23:05:36 +0300

    tests: Promote in the middleend
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- middle/CPSConv.ml
+++ middle/CPSConv.ml
@@ -253,19 +253,19 @@ let%expect_test "counts simple" =
   test_count {| let m x y z = x y y|};
   [%expect
     {|
-    var x got id 71
-    var y got id 72
-    var z got id 73
-    var m got id 74
-    id: 71; counts 1
-    id: 72; counts 2
-    id: 73; counts 0
-    id: 74; counts 0
+    var x got id 73
+    var y got id 74
+    var z got id 75
+    var m got id 76
+    id: 73; counts 1
+    id: 74; counts 2
+    id: 75; counts 0
+    id: 76; counts 0
     ids that ref_once:
-    71
-    ids that no_refs:
     73
-    74
+    ids that no_refs:
+    75
+    76
     |}]
 ;;
 
@@ -273,22 +273,22 @@ let%expect_test "counts branching, shadowing" =
   test_count {| let m x y = if x then fun x -> x 1 else fun x -> (y , y x)|};
   [%expect
     {|
-    var x got id 75
-    var y got id 76
     var x got id 77
-    var x got id 78
-    var m got id 79
-    id: 75; counts 1
-    id: 76; counts 2
+    var y got id 78
+    var x got id 79
+    var x got id 80
+    var m got id 81
     id: 77; counts 1
-    id: 78; counts 1
-    id: 79; counts 0
+    id: 78; counts 2
+    id: 79; counts 1
+    id: 80; counts 1
+    id: 81; counts 0
     ids that ref_once:
-    75
     77
-    78
-    ids that no_refs:
     79
+    80
+    ids that no_refs:
+    81
     |}]
 ;;
 
@@ -296,12 +296,12 @@ let%expect_test "counts rec, ptuple" =
   test_count {| let rec (x,y) = x x y|};
   [%expect
     {|
-    var x got id 80
-    var y got id 81
-    id: 80; counts 2
-    id: 81; counts 1
+    var x got id 82
+    var y got id 83
+    id: 82; counts 2
+    id: 83; counts 1
     ids that ref_once:
-    81
+    83
     ids that no_refs:
     |}]
 ;;
--- middle/mangling.ml
+++ middle/mangling.ml
@@ -167,21 +167,21 @@ let%expect_test "global constant shadowing" =
                   x
 
       mangled stru:
-      let x_id53 =
+      let x_id55 =
                                      10
-                                   let f_id54 () =
-                                     x_id53
-                                   let x_id55 =
-                                     20
-                                   let g_id56 () =
+                                   let f_id56 () =
                                      x_id55
+                                   let x_id57 =
+                                     20
+                                   let g_id58 () =
+                                     x_id57
 
       mangled names:
       {
       	x ~> x_id55;
-      	g ~> g_id56;
-      	x ~> x_id53;
-      	f ~> f_id54;
+      	f ~> f_id56;
+      	x ~> x_id57;
+      	g ~> g_id58;
       } |}]
 ;;
 
@@ -209,12 +209,12 @@ let%expect_test "let rec" =
                 0
 
     mangled stru:
-    let rec fact_id57 n =
+    let rec fact_id59 n =
                                    let temp1 = (n < 1) in
                                    let temp2 = (if temp1
                                                then 1
                                                else let temp3 = (n - 1) in
-                                                    let temp4 = fact_id57 temp3  in
+                                                    let temp4 = fact_id59 temp3  in
                                                     let temp5 = (n * temp4) in
                                                     temp5) in
                                      temp2
@@ -223,8 +223,8 @@ let%expect_test "let rec" =
 
     mangled names:
     {
-    	main ~> main_id59;
-    	fact ~> fact_id57;
+    	main ~> main_id61;
+    	fact ~> fact_id59;
     } |}]
 ;;
 
@@ -254,16 +254,16 @@ let%expect_test "replacing built-in print with user-defined one" =
     let () =
                                        let temp6 = print 1  in
                                        temp6
-                                     let print_id65 weird7 =
+                                     let print_id67 weird7 =
                                        let _ = weird7 in
                                        0
                                      let () =
-                                       let temp8 = print_id65 2  in
+                                       let temp8 = print_id67 2  in
                                        temp8
 
     mangled names:
     {
-    	print ~> print_id65;
+    	print ~> print_id67;
     } |}]
 ;;
 
@@ -294,18 +294,18 @@ let%expect_test "shadowing global function with local one" =
                 temp10
 
     mangled stru:
-    let id_id69 x =
+    let id_id71 x =
                                         x
                                       let () =
                                         let id y = y in
                                           let temp9 = id 0  in
                                           temp9
                                       let () =
-                                        let temp10 = id_id69 0  in
+                                        let temp10 = id_id71 0  in
                                         temp10
 
     mangled names:
     {
-    	id ~> id_id69;
+    	id ~> id_id71;
     } |}]
 ;;
