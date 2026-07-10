53c66e3e23e75fe2606602ad01916fa6eface71a
Kakadu <Kakadu@pm.me>
2026-07-11 00:24:25 +0300

    RV32: add demo project
    Signed-off-by: Kakadu <Kakadu@pm.me>

---
+++ tests/rv32/demo.ml
@@ -0,0 +1,15 @@
+(*
+test
+  (targets rv32 rv64 amd64)
+  (run (stdout "rukaml_print_int 255"))
+*)
+
+let revapply x k =
+   k x
+let const0 x = 0
+let fresh1 z x = z const0
+let z k = 0
+let main =
+  let u = fresh1 z in
+  let w = print 255 in
+  u 1
